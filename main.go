8549412d8c3fbb4b3332d2c185f3ea9e4bd177f1
Hemant Kumar <hemant.kumar2@altair.com>
2023-06-11 11:49:20 +0530

    add rabbitmq queue

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/kmrhemant916/notification/rabbitmq"
 	"github.com/kmrhemant916/notification/utils"
 )
@@ -21,4 +23,42 @@ func main() {
 		panic(err)
 	}
 	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	q, err := ch.QueueDeclare(
+		"mail", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err != nil {
+		panic(err)
+	}
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		panic(err)
+	}
+	var forever chan struct{}
+
+	go func() {
+	  for d := range msgs {
+		log.Printf("Received a message: %s", d.Body)
+	  }
+	}()
+	
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
